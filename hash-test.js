6923b9a0e239a493289d856c612263bb9b4d3c3d
rochelle <loqiu.software@gmail.com>
2025-05-14 20:33:46 +0100

    nextjs

---
+++ hash-test.js
@@ -0,0 +1,10 @@
+import bcrypt from "bcryptjs";
+
+async function test() {
+    try {
+        const res = await bcrypt.hash("123456",10);
+        console.log("res: ", res);
+    } catch (err) {
+        console.error('hash error: ', err)
+    }
+}
